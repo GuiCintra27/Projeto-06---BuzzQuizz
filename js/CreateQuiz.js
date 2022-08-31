a90de9f51e3381b39a28ad0c9f231d3778a90e01
Guilherme Cintra <gui7lk@gmail.com>
2022-08-31 10:40:34 -0300

    Botões da última página funcionando

---
+++ js/CreateQuiz.js
@@ -0,0 +1,23 @@
+function nextSection(section){
+    section.classList.add('Hide');
+    if (section.className.includes('First')){
+        let nextSection = document.querySelector('.Second.Section');
+        nextSection.classList.remove('Hide');
+    }else if (section.className.includes('Second')){
+        let nextSection = document.querySelector('.Third.Section');
+        nextSection.classList.remove('Hide');
+    }else if (section.className.includes('Third')){
+        let nextSection = document.querySelector('.Last.Section');
+        nextSection.classList.remove('Hide');
+    }else if (section.className.includes('Last')){
+        let thisPage = document.querySelector('.Create_quiz');
+        thisPage.classList.add('Hide');
+        let quizPage = document.querySelector('.Quiz_page');
+        quizPage.classList.remove('Hide');
+    }
+}
+
+function goToHome(home, section){
+    section.classList.add('Hide');
+    home.classList.remove('Hide');
+}
