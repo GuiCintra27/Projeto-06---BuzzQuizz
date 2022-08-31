b089015c5a77ce3268d8be0b4ffd38e3da19f3de
Esdras Antero <esdrasanterofontes@gmail.com>
2022-08-31 14:07:29 -0300

    botão para ir pra página do quiz meio pronto

---
+++ js/QuizPage.js
@@ -0,0 +1,7 @@
+function toQuizPage() {
+    const initialPage = document.querySelector("#Select-quiz")
+    const pageQuizTwo = document.querySelector(".Quiz_page")
+
+    initialPage.classList.add('Hide')
+    pageQuizTwo.classList.remove("Hide")
+}
