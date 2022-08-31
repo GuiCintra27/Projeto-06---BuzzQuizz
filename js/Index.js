1c746c46f5b14bb5d5ea69695bfb6f27f7d1af41
caiohbe <caiohbe@gmail.com>
2022-08-31 14:31:52 -0300

    Renderização dos quizzes da API/quizzes clicaveis

--- js/Index.js
+++ js/Index.js
@@ -1,3 +1,40 @@
-function TESTE() {
-    
+// Renderizando os quizzes da API
+let selectedQuiz 
+let quizzesData
+
+const quizPage = document.querySelector('.Quiz_page')
+const homePage = document.querySelector('#Select-quiz')
+
+axios
+.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
+.then((quizzes) => {
+    quizzesData = quizzes.data;
+    console.log(quizzes.data) // CONSOLE.LOG
+    const divs = quizzesData.map((quiz) => {
+        return `
+        <div class="Quiz" onclick="acessQuiz(${quiz.id})">
+            <h4>${quiz.title}</h4>
+        </div>
+        `;
+    });
+    let allQuizzes = document.querySelector('.Quiz_container');
+    allQuizzes.innerHTML = ''; // Essa linha é removível depois de tirarmos os quizzes estáticos do HTML
+    for(let i = 0; i < divs.length; i++) {
+        allQuizzes.innerHTML += divs[i];
+    }
+});
+
+// Função para renderizar o quiz clicado
+function acessQuiz(quizId) {
+    selectedQuiz = quizzesData.filter((quizzesData) => {
+        if (quizzesData.id == quizId) {
+            return true
+        }
+    })
+
+    homePage.classList.add('Hide')
+    quizPage.classList.remove('Hide')
+
+    const header = document.querySelector('header')
+    header.scrollIntoView()
 }
